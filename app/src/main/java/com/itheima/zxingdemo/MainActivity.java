b731fc0cf5454e528dec8e35ad333e0f9de48c37
open-android <itcastsh@126.com>
2017-01-11 00:04:06 +0800

    add jitpack

--- app/src/main/java/com/itheima/zxingdemo/MainActivity.java
+++ app/src/main/java/com/itheima/zxingdemo/MainActivity.java
@@ -9,14 +9,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.EncodeHintType;
-import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.activity.CaptureActivity;
-import com.google.zxing.common.BitMatrix;
-
-import java.util.Hashtable;
+import com.google.zxing.common.BitmapUtils;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -51,44 +46,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.btn2:
                 String content = mEt.getText().toString().trim();
-                try {
-                    Bitmap bitmap = Create2DCode(content);
-                    mImage.setImageBitmap(bitmap);
-                } catch (WriterException e) {
-                    e.printStackTrace();
-                }
-                break;
+//                try {
+//                    Bitmap bitmap = null;
+//                    mImage.setImageBitmap(bitmap);
+//                } catch (WriterException e) {
+//                    e.printStackTrace();
+//                }
+//                break;
         }
     }
 
-    /**
-     * 用字符串生成二维码
-     *
-     * @param str
-     * @return
-     * @throws WriterException
-     */
-    public Bitmap Create2DCode(String str) throws WriterException {
-        Hashtable<EncodeHintType, String> hints = new Hashtable<EncodeHintType, String>();
-        hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
-        // 生成二维矩阵,编码时指定大小,不要生成了图片以后再进行缩放,这样会模糊导致识别失败
-        BitMatrix matrix = new MultiFormatWriter().encode(str, BarcodeFormat.QR_CODE, 480, 480, hints);
-        int width = matrix.getWidth();
-        int height = matrix.getHeight();
-        // 二维矩阵转为一维像素数组,也就是一直横着排了
-        int[] pixels = new int[width * height];
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                if (matrix.get(x, y)) {
-                    pixels[y * width + x] = 0xff000000;
-                }
-            }
-        }
-
-        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-        // 通过像素数组生成bitmap,具体参考api
-        bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
-        return bitmap;
-    }
 
 }
--- zxinglib/src/main/java/com/google/zxing/common/BitmapUtils.java
+++ zxinglib/src/main/java/com/google/zxing/common/BitmapUtils.java
@@ -4,6 +4,13 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+
+import java.util.Hashtable;
+
 public class BitmapUtils {
 
 	public static Bitmap decodeSampledBitmapFromResource(Resources res,
@@ -55,5 +62,35 @@ public class BitmapUtils {
 		options.inSampleSize = calculateInSampleSize(options, 480, 800);
 		options.inJustDecodeBounds = false;
 		return BitmapFactory.decodeFile(path, options);
+	}
+
+	/**
+	 * 用字符串生成二维码
+	 *
+	 * @param str
+	 * @return
+	 * @throws WriterException
+	 */
+	public static Bitmap create2DCode(String str) throws WriterException {
+		Hashtable<EncodeHintType, String> hints = new Hashtable<EncodeHintType, String>();
+		hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+		// 生成二维矩阵,编码时指定大小,不要生成了图片以后再进行缩放,这样会模糊导致识别失败
+		BitMatrix matrix = new MultiFormatWriter().encode(str, BarcodeFormat.QR_CODE, 480, 480, hints);
+		int width = matrix.getWidth();
+		int height = matrix.getHeight();
+		// 二维矩阵转为一维像素数组,也就是一直横着排了
+		int[] pixels = new int[width * height];
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				if (matrix.get(x, y)) {
+					pixels[y * width + x] = 0xff000000;
+				}
+			}
+		}
+
+		Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+		// 通过像素数组生成bitmap,具体参考api
+		bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+		return bitmap;
 	}
 }
